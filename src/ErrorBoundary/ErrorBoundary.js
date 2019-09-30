b21cc449da96f79ec79caae30a4effd40004141b
Sanath Shetty <53327163+Sanathshetty98@users.noreply.github.com>
2019-09-30 08:25:46 +0530

    Add files via upload

---
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React,{ Component} from 'react';
+class ErrorBoundary extends Component{
+
+  state={
+    hasError:false,
+    ErrorMessage:''
+  }
+
+  compenentDidCatch = (error,info) => {
+      this.setState({hasError: true , ErrorMessage: error});
+  }
+  render(){
+    if(this.state.hasError){
+      return <h1>{this.state.ErrorMessage}</h1>
+    }
+    else{
+      return this.props.children;
+    }
+  }
+}
+
+export default ErrorBoundary;
